9633bc45ce441f030650b01894333f3c01e57b04
Jacob <wittenberg.jacob@gmail.com>
2019-11-02 12:52:57 -0500

    got all snack positions

---
+++ src/javascripts/components/machine/machine.js
@@ -0,0 +1,17 @@
+import smash from '../../helpers/data/smash';
+
+import './machine.scss';
+
+const buildTheMachine = () => {
+  smash.getCompleteMachine()
+    .then((singleMachine) => console.log('1 machine', singleMachine))
+    .catch((error) => console.error(error));
+  // 1. getMachines - returns first machine (hard coding) - DONE
+  // 2. use machineId to get all positions for that machine
+  // 3. use machineId to get all snack positions
+  // 4. use uid of snackPositions/positions to get available snacks for machine
+  // 5. Smash em - return an array of posiitons (in order A1, A2, A3, etc)
+  // so positions should have position.snack
+};
+
+export default { buildTheMachine };
---
+++ src/javascripts/helpers/data/smash.js
@@ -0,0 +1,15 @@
+import machineData from './machineData';
+import positionData from './positionData';
+import snackPositionData from './snackPositionData';
+
+const getCompleteMachine = () => new Promise((resolve, reject) => {
+  machineData.getMachine()
+    .then((singleMachine) => positionData.getAllPositionsByMachineId(singleMachine.id))
+    .then((positions) => {
+      snackPositionData.getAllSnackPositionsByMachineId(positions[0].machineId)
+        .then((snackPositions) => resolve(snackPositions));
+    })
+    .catch((error) => reject(error));
+});
+
+export default { getCompleteMachine };
