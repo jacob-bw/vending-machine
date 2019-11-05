ef7ffae579a789156530b9295ea3c7b4b19d93e1
Jacob <wittenberg.jacob@gmail.com>
2019-11-04 19:46:37 -0600

    delete works

--- src/javascripts/components/stocker/stocker.js
+++ src/javascripts/components/stocker/stocker.js
@@ -1,9 +1,24 @@
+import $ from 'jquery';
+import firebase from 'firebase/app';
+import 'firebase/auth';
 import smash from '../../helpers/data/smash';
-
 import utilities from '../../helpers/utilities';
 import stockCard from '../stockCard/stockCard';
-
 import './stocker.scss';
+import snackPositionData from '../../helpers/data/snackPositionData';
+import machine from '../machine/machine';
+
+const deleteFromMachine = (e) => {
+  e.preventDefault();
+  const { uid } = firebase.auth().currentUser;
+  snackPositionData.deleteSnackPosition(e.target.id)
+    .then(() => {
+      // eslint-disable-next-line no-use-before-define
+      buildTheStocker(uid);
+      machine.buildTheMachine();
+    })
+    .catch((error) => console.error(error));
+};
 
 const buildTheStocker = (uid) => {
 //  console.log('uid in buildTheStocker', uid);
@@ -16,6 +31,7 @@ const buildTheStocker = (uid) => {
       });
       domString += '</div>';
       utilities.printToDom('stock', domString);
+      $('#stock').on('click', '.delete-snack-position', deleteFromMachine);
     })
     .catch((error) => console.error(error));
 };
