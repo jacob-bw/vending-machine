a3dc72c3dc3d31c056759b91d5584a33d2a17207
Jacob <wittenberg.jacob@gmail.com>
2019-11-04 19:20:21 -0600

    READ added to machine

---
+++ src/javascripts/components/stocker/stocker.js
@@ -0,0 +1,23 @@
+import smash from '../../helpers/data/smash';
+
+import utilities from '../../helpers/utilities';
+import stockCard from '../stockCard/stockCard';
+
+import './stocker.scss';
+
+const buildTheStocker = (uid) => {
+//  console.log('uid in buildTheStocker', uid);
+  smash.getSnacksWithPositions(uid)
+    .then((snacks) => {
+      let domString = '<h2>STOCK THE MACHINE</h2>';
+      domString += '<div class="d-flex flex-wrap">';
+      snacks.forEach((snack) => {
+        domString += stockCard.makeASnack(snack);
+      });
+      domString += '</div>';
+      utilities.printToDom('stock', domString);
+    })
+    .catch((error) => console.error(error));
+};
+
+export default { buildTheStocker };
--- src/javascripts/helpers/data/authData.js
+++ src/javascripts/helpers/data/authData.js
@@ -1,6 +1,7 @@
 import $ from 'jquery';
 import firebase from 'firebase/app';
 import 'firebase/auth';
+import stocker from '../../components/stocker/stocker';
 
 const authDiv = $('#auth');
 const stockDiv = $('#stock');
@@ -12,6 +13,7 @@ const checkLoginStatus = () => {
       stockDiv.removeClass('hide');
       logoutNavbar.removeClass('hide');
       authDiv.addClass('hide');
+      stocker.buildTheStocker(user.uid);
     } else {
       stockDiv.addClass('hide');
       logoutNavbar.addClass('hide');
